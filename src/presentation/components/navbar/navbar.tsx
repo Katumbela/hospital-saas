8d2e6d84b2571fb75389d894a9c935aa727009d2
João Afonso Katombela <ja3328173@gmail.com>
2024-05-27 14:20:23 +0100

    from rcs pc

--- src/presentation/components/navbar/navbar.tsx
+++ src/presentation/components/navbar/navbar.tsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from "react";
 import { FaAngleDown, FaUserCircle } from "react-icons/fa";
-import { AnimatePresence, motion } from "framer-motion"; 
+import { AnimatePresence, motion } from "framer-motion";
 import { BsBell } from "react-icons/bs";
+import { FaArrowRightFromBracket, FaGear } from "react-icons/fa6";
 
 export type NavBarContentProps = {
   activeLink: string;
@@ -59,6 +60,7 @@ export function NavBar() {
 }
 
 function NavBarContent() {
+  const [itemClicked, setitemClicked] = useState("");
   return (
     <div className="flex justify-between containerr px-[2.5rem]">
       <div className="flex gap-5 logo">
@@ -72,18 +74,75 @@ function NavBarContent() {
       </div>
 
       <div className="flex gap-4">
-        <button className="flex gap-3 px-4 py-2 my-auto text-sm font-medium transition-all bg-white rounded-md hover:bg-white/90">
-          Katombela Demo <FaAngleDown className="mt-[5px] " />
-        </button>
-        <button className="ms-[2rem] cursor-pointer border border-transparent hover:border-white transition-all  py-[.4rem] rounded-full px-[.4rem] bg-white/40 my-auto">
-          <BsBell className="my-auto text-2xl text-white" />
-        </button>
-        <div className="flex cursor-pointer border border-transparent hover:border-white transition-all p-1.5 gap-1 rounded-full bg-white/40">
-          <FaUserCircle className="my-auto text-2xl text-white" />
-          <span className="my-auto text-sm font-semibold text-white">
-            Dr. João Katombela
-          </span>
-          <FaAngleDown className="mt-[7px] text-white me-1" />
+        <div className="relative">
+          <button
+            onClick={() => setitemClicked(itemClicked === "demo" ? "" : "demo")}
+            className="flex gap-3 px-4 py-2 my-auto text-sm font-medium transition-all bg-white rounded-md hover:bg-white/90"
+          >
+            Katombela Demo <FaAngleDown className="mt-[5px] " />
+          </button>
+          {itemClicked === "demo" && (
+            <button className="absolute flex w-full gap-3 px-4 py-2 my-auto text-sm font-medium transition-all bg-purple-100 shadow-lg rounded-s hover:bg-purple-200">
+              Update to <b className="text-purple-800">PRO</b>
+            </button>
+          )}
+        </div>
+        <div className="relative">
+          <button
+            onClick={() => setitemClicked(itemClicked === "bell" ? "" : "bell")}
+            className={`ms-[2rem] click cursor-pointer border border-transparent hover:border-white transition-all  py-[.4rem] rounded-full px-[.4rem]  my-auto ${
+              itemClicked === "bell"
+                ? "bg-white text-purple-800"
+                : "text-white bg-white/40"
+            }`}
+          >
+            <BsBell className="my-auto text-2xl " />
+          </button>
+          {itemClicked === "bell" && (
+            <div className="absolute  top-[2.7rem] bg-white z-50 w-[14rem] right-0 p-3 shadow-xl">
+              <b>Notifications</b>
+              <p className="p-1 mt-2 text-xs rounded-sm bg-slate-200">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea,
+                porro perferendis!
+              </p>
+              <p className="p-1 mt-2 text-xs rounded-sm bg-slate-200">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea,
+                porro perferendis!
+              </p>
+              <p className="p-1 mt-2 text-xs rounded-sm bg-slate-200">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea,
+                porro perferendis!
+              </p>
+            </div>
+          )}
+        </div>
+
+        <div className="relative">
+          <div
+            onClick={() =>
+              setitemClicked(itemClicked === "profile" ? "" : "profile")
+            }
+            className="flex click cursor-pointer border border-transparent hover:border-white transition-all p-1.5 gap-1 rounded-full bg-white/40"
+          >
+            <FaUserCircle className="my-auto text-2xl text-white" />
+            <span className="my-auto text-sm font-semibold text-white">
+              Dr. João Katombela
+            </span>
+            <FaAngleDown className="mt-[7px] text-white me-1" />
+          </div>
+          {itemClicked === "profile" && (
+            <div className="absolute rounded-md bg-white top-[2.7rem] z-50 w-[14rem] right-0 p-3 shadow-xl">
+              <p className="py-1.5 px-2 mt-2 flex gap-2 text-sm rounded-sm hover:bg-slate-300 transition-all cursor-pointer bg-slate-200">
+                <FaUserCircle className="my-auto" /> View Profile
+              </p>
+              <p className="py-1.5 px-2 mt-2 flex gap-2 text-sm rounded-sm hover:bg-slate-300 transition-all cursor-pointer bg-slate-200">
+                <FaGear className="my-auto" /> Settings
+              </p>
+              <p className="py-1.5 px-2 mt-2 flex gap-2 text-sm rounded-sm hover:bg-slate-300 transition-all cursor-pointer bg-slate-200">
+              <FaArrowRightFromBracket className="my-auto" /> Logout
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </div>
--- src/presentation/components/patient-table/patient-table.tsx
+++ src/presentation/components/patient-table/patient-table.tsx
@@ -93,10 +93,10 @@ export const PatientTable: React.FC<PatientTableProps> = ({
       <table className="w-auto text-sm table-fixed text-start text-surface">
         <thead className="font-medium border-b border-neutral-200 dark:border-white/10">
           <tr className="bg-violett/10">
-            <th scope="col" className="w-16 px-2 py-4">
+            <th scope="col" className="px-2 py-4 w-[2rem]">
               NO #
             </th>
-            <th scope="col" className="px-2 text-start w-[11rem] py-2">
+            <th scope="col" className="px-2 text-start w-[10rem] py-2">
               Patient Details
             </th>
             <th scope="col" className="px-2 py-2 w-[6rem] text-start">
@@ -107,7 +107,7 @@ export const PatientTable: React.FC<PatientTableProps> = ({
             </th>
             <th
               scope="col"
-              className="px-2 text-violett w-[6.6rem] py-2 text-start"
+              className="px-2 text-violett w-[6rem] py-2 text-start"
             >
               <span className="flex gap-2">
                 <span>Case</span>
@@ -116,16 +116,16 @@ export const PatientTable: React.FC<PatientTableProps> = ({
                 </div>
               </span>
             </th>
-            <th scope="col" className="px-2 w-[8.5rem] py-2 text-start">
+            <th scope="col" className="px-2 w-[8rem] py-2 text-start">
               Category
             </th>
-            <th scope="col" className="px-2 w-[7rem] py-2 text-start">
+            <th scope="col" className="px-2 w-[6.5rem] py-2 text-start">
               Referred By
             </th>
-            <th scope="col" className="px-2 w-[6rem] py-2 text-start">
+            <th scope="col" className="px-2 w-[5.5rem] py-2 text-start">
               Time
             </th>
-            <th scope="col" className="px-2 w-[9rem] py-2 text-start">
+            <th scope="col" className="px-2 w-[8.5rem] py-2 text-start">
               <span className="flex gap-2">
                 <span>Status</span>
                 <div className="border border-violett rounded-sm p-[2.5px] my-auto">
@@ -133,7 +133,7 @@ export const PatientTable: React.FC<PatientTableProps> = ({
                 </div>
               </span>
             </th>
-            <th scope="col" className="px-2 py-2 text-start">
+            <th scope="col" className="w-[15rem] px-2 py-2 text-start">
               Action
             </th>
           </tr>
